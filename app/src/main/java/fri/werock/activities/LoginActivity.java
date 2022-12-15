229b6d491ebaf89a3f44fc48f5756a6c61c4fbdf
chemala <aladinko4@gmail.com>
2022-12-15 01:45:18 +0100

    Edited Login and added dummy error messages

--- app/src/main/java/fri/werock/activities/LoginActivity.java
+++ app/src/main/java/fri/werock/activities/LoginActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import fri.werock.R;
@@ -20,10 +22,16 @@ public class LoginActivity extends AppCompatActivity {
     private EditText editUsername;
     private EditText editPassword;
 
+    private TextView usernameErr;
+    private TextView passwordErr;
+
+    private ImageView layers;
+
     private Button buttonLogin;
     private Button buttonRegister;
+    public String user = "User";
+    public String password = "Pass";
 
-    private TextView testText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +41,15 @@ public class LoginActivity extends AppCompatActivity {
         UserTokenStorage userTokenStorage = new UserTokenStorage(LoginActivity.this);
         WeRockApi weRockApi = WeRockApi.create(this);
 
+        this.layers = this.findViewById(R.id.layers);
+        this.layers.setImageResource(R.drawable.layers);
+
         this.editUsername = this.findViewById(R.id.edit_username);
         this.editPassword = this.findViewById(R.id.edit_password);
 
-        //this.testText = this.findViewById(R.id.testText);
+        this.usernameErr = this.findViewById(R.id.userErr);
+        this.passwordErr = this.findViewById(R.id.passErr);
+
 
         this.buttonLogin = this.findViewById(R.id.login_button);
         this.buttonLogin.setOnClickListener(view -> {
@@ -53,7 +66,18 @@ public class LoginActivity extends AppCompatActivity {
 
                 @Override
                 public void onError(ApiError error) {
-                    //TODO: Response errors
+                    //If username not found
+                    if(!userAccount.getUsername().equals(user)) {
+                        usernameErr.setVisibility(View.VISIBLE);
+                        usernameErr.setText("Username not found");
+                    }
+
+                    if(!userAccount.getPassword().equals(password)) {
+                        passwordErr.setVisibility(View.VISIBLE);
+                        passwordErr.setText("Incorrect password");
+                    }
+
+
                 }
 
                 @Override
