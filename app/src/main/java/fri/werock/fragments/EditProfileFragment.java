54e9679085422a34eb9ff75c686b97cb7a44c45d
chemala <aladinko4@gmail.com>
2023-01-07 19:46:25 +0100

    Changed EditProfileFragment

--- app/src/main/java/fri/werock/fragments/EditProfileFragment.java
+++ app/src/main/java/fri/werock/fragments/EditProfileFragment.java
@@ -1,23 +1,61 @@
 package fri.werock.fragments;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.webkit.URLUtil;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import fri.werock.R;
 import fri.werock.activities.AuthenticatedActivity;
 import fri.werock.activities.LoginActivity;
+import fri.werock.utils.AudioPlayer;
+
+import static android.app.Activity.RESULT_OK;
 
 public class EditProfileFragment extends Fragment {
     private Button logout;
+    private TextView addMedia, editPicture;
+    private ConstraintLayout layout;
+
+    private EditText aboutMe, editTags, editLink;
+    private ImageView myProfileImg;
+
+    private Button backButton;
+
+    private final int PICK_AUDIO = 1;
+    private final int PICK_IMAGE = 2;
+    Uri AudioUri;
+    AudioPlayer audioPlayer = new AudioPlayer(this.getContext());
 
     public EditProfileFragment() {}
 
@@ -47,10 +85,181 @@ public class EditProfileFragment extends Fragment {
             return;
         }
 
+        super.onCreate(savedInstanceState);
+
+        addMedia = this.getActivity().findViewById(R.id.addMedia);
+        editPicture = this.getActivity().findViewById(R.id.edit_profile_pic);
+        layout = this.getActivity().findViewById(R.id.audioContainer);
+
+        aboutMe = this.getActivity().findViewById(R.id.aboutme_txt_input);
+        editTags = this.getActivity().findViewById(R.id.edit_tags_input);
+        myProfileImg = this.getActivity().findViewById(R.id.myProfileImg);
+        editLink = this.getActivity().findViewById(R.id.videoLink);
+
+
+
+        aboutMe.setOnFocusChangeListener((v, b) -> {
+            if(b) {
+                aboutMe.setGravity(Gravity.TOP);
+                ViewGroup.LayoutParams params = aboutMe.getLayoutParams();
+                params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                params.height = 300;
+                aboutMe.setLayoutParams(params);
+            }else{
+                aboutMe.setGravity(Gravity.NO_GRAVITY);
+                ViewGroup.LayoutParams params = aboutMe.getLayoutParams();
+                params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+                aboutMe.setLayoutParams(params);
+            }
+        });
+
+
+        editLink.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(URLUtil.isValidUrl(charSequence.toString())){
+                    editLink.setTextColor(getResources().getColor(R.color.add_media_color));
+                }else{
+                    editLink.setTextColor(getResources().getColor(R.color.black));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
+        editTags.setOnFocusChangeListener((v, b) -> {
+            if(!b){
+                List<String> tags = parseTags(editTags.getText().toString());
+                editTags.setText(tags.toString().substring(1,tags.toString().length()-1));
+                editTags.setTextColor(this.getActivity().getColor(R.color.add_media_color));
+            }else{
+                editTags.setTextColor(this.getActivity().getColor(R.color.black));
+            }
+        });
+
+        editPicture.setOnClickListener(v -> {
+            Intent image = new Intent();
+            image.setType("image/*");
+            image.setAction(Intent.ACTION_PICK);
+            startActivityForResult(Intent.createChooser(image, "Select Image"), PICK_IMAGE);
+
+        });
+
+
+
+
+        addMedia.setOnClickListener(v-> {
+            if(layout.getChildCount()<2) {
+                Intent audio = new Intent();
+                audio.setType("audio/*");
+                audio.setAction(Intent.ACTION_OPEN_DOCUMENT);
+                startActivityForResult(Intent.createChooser(audio, "Select Audio"), PICK_AUDIO);
+            }else{
+
+                Toast.makeText(this.getActivity(), "Max number of demos reached", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+
+
+
+
         this.logout = this.getActivity().findViewById(R.id.logout);
         logout.setOnClickListener(buttonView -> {
             activity.getUserTokenStorage().clear();
             startActivity(new Intent(activity.getApplicationContext(), LoginActivity.class));
         });
     }
+
+    public void onActivityResult ( int requestCode, int resultCode, @Nullable Intent data){
+
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("CODE", String.valueOf(requestCode));
+        if(requestCode == PICK_AUDIO) {
+            if (resultCode == RESULT_OK) {
+                // Audio is Picked in format of URI
+                AudioUri = data.getData();
+                MediaPlayer mediaPlayer = MediaPlayer.create(this.getActivity().getApplicationContext(), AudioUri);
+                if (mediaPlayer.getDuration() < 60000) {
+                    View player = getLayoutInflater().inflate(R.layout.audio_player_layout, null);
+                    audioPlayer.addPlayer(player, mediaPlayer, layout);
+
+                } else {
+                    showDialog();
+                }
+
+            }
+        }
+        if(requestCode == PICK_IMAGE) {
+
+            if (resultCode == RESULT_OK) {
+                try {
+                    final Uri imageUri = data.getData();
+                    final InputStream imageStream = this.getActivity().getContentResolver().openInputStream(imageUri);
+                    final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                    myProfileImg.setImageBitmap(selectedImage);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Toast.makeText(this.getActivity(), "Something went wrong", Toast.LENGTH_LONG).show();
+                }
+
+            } else {
+                Toast.makeText(this.getActivity(), "You haven't picked an image", Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
+
+    void showDialog(){
+        final Dialog dialog = new Dialog(this.getActivity());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(true);
+        dialog.setContentView(R.layout.clip_too_big_dialog);
+
+
+        Button closeButton = dialog.findViewById(R.id.clip_dia_close);
+
+        closeButton.setOnClickListener(view -> {
+            dialog.dismiss();
+        });
+
+        dialog.show();
+    }
+
+    public String parseYtLink(String url){
+        String pattern = "https?:\\/\\/(?:[0-9A-Z-]+\\.)?(?:youtu\\.be\\/|youtube\\.com\\S*[^\\w\\-\\s])([\\w\\-]{11})(?=[^\\w\\-]|$)(?![?=&+%\\w]*(?:['\"][^<>]*>|<\\/a>))[?=&+%\\w]*";
+
+        Pattern compiledPattern = Pattern.compile(pattern,
+                Pattern.CASE_INSENSITIVE);
+        Matcher matcher = compiledPattern.matcher(url);
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+        return null;
+    }
+
+    public static List<String> parseTags(final String input) {
+
+        List<String> allMatches = new ArrayList<String>();
+        final Pattern pattern = Pattern.compile("\\B#([a-z0-9]{2,})(?![~!@#$%^&*()=+_`\\-\\|\\/'\\[\\]\\{\\}]|[?.,]*\\w)", Pattern.CASE_INSENSITIVE);
+        final Matcher matcher = pattern.matcher(input);
+        while (matcher.find()) {
+            allMatches.add(matcher.group());
+        }
+        return allMatches;
+    }
+
 }
